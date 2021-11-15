36627d6ef4d1d9b5fc3dd8a651236f12f4ba8c9e
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-15 18:58:42 +0100

    aggiunta area di testo messaggi

--- js/script.js
+++ js/script.js
@@ -2,4 +2,33 @@ const app = new Vue({
 
   el:'#app',
 
+  data:{
+    users:[
+      {
+        img: 'img/avatar_1.jpg',
+        nome: 'Michele',
+        msg: 'Tutto fatto',
+        ultimoAccesso: '10/01/2020 16:15:22'
+      },
+      {
+        img: 'img/avatar_2.jpg',
+        nome: 'Fabio',
+        msg: 'Mi piacerebbe ma devo andare a ...',
+        ultimoAccesso: '20/03/2020 16:35:00'
+      },
+      {
+        img: 'img/avatar_3.jpg',
+        nome: 'Samuele',
+        msg: 'Ah scusa! ...',
+        ultimoAccesso: '28/03/2020 16:15:22'
+      },
+      {
+        img: 'img/avatar_4.jpg',
+        nome: 'Luisa',
+        msg: 'Si, ma preferirei andare al ci ...',
+        ultimoAccesso: '10/01/2020 15:50:00'
+      },
+    ]
+  }
+
 })
