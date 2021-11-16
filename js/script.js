4e396876d8f000b833115157ef26428d79395f5a
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-16 17:26:10 +0100

    inizio chat messaggi

--- js/script.js
+++ js/script.js
@@ -3,32 +3,92 @@ const app = new Vue({
   el:'#app',
 
   data:{
-    users:[
+    contacts: [
       {
-        img: 'img/avatar_1.jpg',
-        nome: 'Michele',
-        msg: 'Tutto fatto',
-        ultimoAccesso: '10/01/2020 16:15:22'
+        name: 'Michele',
+        avatar: 'img/avatar_1.jpg',
+        visible: true,
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          message: 'Hai portato a spasso il cane?',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          message: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 16:15:22',
+          message: 'Tutto fatto!',
+          status: 'received'
+        }],
       },
       {
-        img: 'img/avatar_2.jpg',
-        nome: 'Fabio',
-        msg: 'Mi piacerebbe ma devo andare a ...',
-        ultimoAccesso: '20/03/2020 16:35:00'
+        name: 'Fabio',
+        avatar: 'img/avatar_2.jpg',
+        visible: true,
+        messages: [{
+          date: '20/03/2020 16:30:00',
+          message: 'Ciao come stai?',
+          status: 'sent'
+        },
+        {
+          date: '20/03/2020 16:30:55',
+          message: 'Bene grazie! Stasera ci vediamo?',
+          status: 'received'
+        },
+        {
+          date: '20/03/2020 16:35:00',
+          message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+          status: 'received'
+        }],
       },
       {
-        img: 'img/avatar_3.jpg',
-        nome: 'Samuele',
-        msg: 'Ah scusa! ...',
-        ultimoAccesso: '28/03/2020 16:15:22'
+        name: 'Samuele',
+        avatar: 'img/avatar_3.jpg',
+        visible: true,
+        messages: [{
+          date: '28/03/2020 10:10:40',
+          message: 'La Marianna va in campagna',
+          status: 'received'
+        },
+        {
+          date: '28/03/2020 10:20:10',
+          message: 'Sicuro di non aver sbagliato chat?',
+          status: 'sent'
+        },
+        {
+          date: '28/03/2020 16:15:22',
+          message: 'Ah scusa!',
+          status: 'received'
+        }],
       },
       {
-        img: 'img/avatar_4.jpg',
-        nome: 'Luisa',
-        msg: 'Si, ma preferirei andare al ci ...',
-        ultimoAccesso: '10/01/2020 15:50:00'
+        name: 'Luisa',
+        avatar: 'img/avatar_4.jpg',
+        visible: true,
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          message: 'Lo sai che ha aperto una nuova pizzeria?',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          message: 'Si, ma preferirei andare al cinema',
+          status: 'received'
+        }],
       },
-    ]
-  }
+    ],
 
+    activeUser: -1
+  },
+
+  methods:{
+    showName(index){
+      console.log('utente attivo', index);
+      this.activeUser = index;
+
+    }
+  }
 })
